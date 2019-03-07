047f70c4ad959fe5e77822d21fbab2e8b0b36888
Danail Zlatarov <38438268+dzlatarov@users.noreply.github.com>
2019-03-07 16:21:31 +0200

    Add files via upload

---
+++ 02. Encapsulation Lab/02. Salary Increase/Person.cs
@@ -0,0 +1,88 @@
+﻿
+namespace PersonsInfo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.salary = salary;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+            set
+            {
+                this.firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+            set
+            {
+                this.lastName = value;
+            }
+        }
+        
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                this.age = value;
+            }
+        }
+
+        public decimal Salary
+        {
+            get
+            {
+                return this.salary;
+            }
+            set
+            {
+                this.salary = value;
+            }
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if(this.Age > 30)
+            {
+                this.salary += this.salary * percentage / 100;
+            }
+            else
+            {
+                this.salary += this.salary * (percentage / 100 / 2);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} receives {this.Salary:f2} leva.";
+        }
+    }
+}
---
+++ 02. Encapsulation Lab/04. First and Reserve Team/Team.cs
@@ -0,0 +1,55 @@
+﻿
+namespace PersonsInfo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+            this.name = name;
+            this.firstTeam = new List<Person>();
+            this.reserveTeam = new List<Person>();
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            private set
+            {
+                this.name = value;
+            }
+        }
+
+        public List<Person> FirstTeam
+        {
+            get { return this.firstTeam; }
+        }
+
+        public List<Person> ReserveTeam
+        {
+            get { return this.reserveTeam; }
+        }
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+            {
+                this.firstTeam.Add(person);
+            }
+            else
+            {
+                this.reserveTeam.Add(person);
+            }
+        }
+    }
+}
