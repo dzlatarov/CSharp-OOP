72f021a41b2d9cfa0e7ff4b450b775dae797a3c5
Danail Zlatarov <38438268+dzlatarov@users.noreply.github.com>
2019-03-15 10:39:28 +0200

    Add files via upload

---
+++ 05. Polimorphism Lab/02. Animals/Cat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Cat : Animal
+    {
+        public Cat(string name, string favouriteFood) 
+            : base(name, favouriteFood)
+        {
+
+        }
+
+        public override string ExplainSelf()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine(base.ExplainSelf());
+            sb.Append("MEEOW");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ 05. Polimorphism Lab/03. Shapes/Circle.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public override double CalculateArea()
+        {
+            return this.radius * radius * Math.PI;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return this.radius * Math.PI * 2;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + "Circle";
+        }
+    }
+}
