c783cd1cbfff5e53c1f53ab84dbdce7adc8a54fe
Danail Zlatarov <38438268+dzlatarov@users.noreply.github.com>
2019-03-25 14:08:44 +0200

    Add files via upload

---
+++ 07. Reflection And Attributes Lab/01. Stealer/Spy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+
+public class Spy
+{
+    public string StealFieldInfo(string className, params string[] searchedFields)
+    {
+        Type classType = Type.GetType(className);
+        var fields = classType.GetFields(
+            BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public);
+        var stringBuilder = new StringBuilder();
+
+        var classInstance = Activator.CreateInstance(classType, new object[] { });
+
+        stringBuilder.AppendLine($"Class under investigation: {className}");
+
+        foreach (FieldInfo field in fields.Where(f => searchedFields.Contains(f.Name)))
+        {
+            stringBuilder.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+        }
+
+        return stringBuilder.ToString().Trim();
+    }
+}
+
---
+++ 07. Reflection And Attributes Lab/03. Mission Private Impossible/Spy.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+
+public class Spy
+{
+    public string StealFieldInfo(string className, params string[] searchedFields)
+    {
+        Type classType = Type.GetType(className);
+        var fields = classType.GetFields(
+            BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public);
+        var stringBuilder = new StringBuilder();
+
+        var classInstance = Activator.CreateInstance(classType, new object[] { });
+
+        stringBuilder.AppendLine($"Class under investigation: {className}");
+
+        foreach (FieldInfo field in fields.Where(f => searchedFields.Contains(f.Name)))
+        {
+            stringBuilder.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+        }
+
+        return stringBuilder.ToString().Trim();
+    }
+
+    public string AnalyzeAcessModifiers(string className)
+    {
+        Type classType = Type.GetType(className);
+        FieldInfo[] classFields = classType.GetFields(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public);
+        MethodInfo[] classPublicMethods = classType.GetMethods(BindingFlags.Instance | BindingFlags.Public);
+        MethodInfo[] classNonPublicMethods = classType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+
+        var sb = new StringBuilder();
+
+        foreach (FieldInfo field in classFields)
+        {
+            sb.AppendLine($"{field.Name} must be private!");
+        }
+
+        foreach (MethodInfo method in classNonPublicMethods.Where(m => m.Name.StartsWith("get")))
+        {
+            sb.AppendLine($"{method.Name} have to be public!");
+        }
+
+        foreach (MethodInfo method in classPublicMethods.Where(m => m.Name.StartsWith("set")))
+        {
+            sb.AppendLine($"{method.Name} have to be private!");
+        }
+
+        return sb.ToString().Trim();
+    }
+
+    public string RevealPrivateMethods(string className)
+    {
+        Type classType = Type.GetType(className);
+        MethodInfo[] classMethods = classType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+        var sb = new StringBuilder();
+
+        sb.AppendLine($"All Private Methods of Class: {className}");
+        sb.AppendLine($"Base Class: {classType.BaseType.Name}");
+
+        foreach (MethodInfo method in classMethods)
+        {
+            sb.AppendLine(method.Name);
+        }
+
+        return sb.ToString().Trim();
+    }
+}
+
