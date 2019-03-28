e5aa8fd1d2485890ebf30cfd6b0a9c8e4d7710a7
Danail Zlatarov <38438268+dzlatarov@users.noreply.github.com>
2019-03-28 14:30:50 +0200

    Add files via upload

---
+++ Exam Retake 19 December 2018/Core/RestaurantController.cs
@@ -0,0 +1,145 @@
+﻿namespace SoftUniRestaurant.Core
+{
+    using SoftUniRestaurant.Models.Drinks.Contracts;
+    using SoftUniRestaurant.Models.Drinks.Factory;
+    using SoftUniRestaurant.Models.Foods.Contracts;
+    using SoftUniRestaurant.Models.Foods.Factory;
+    using SoftUniRestaurant.Models.Tables.Contracts;
+    using SoftUniRestaurant.Models.Tables.Factory;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class RestaurantController
+    {
+        private List<IFood> menu;
+        private List<IDrink> drinks;
+        private List<ITable> tables;
+        private FoodFactory foodFactory;
+        private DrinkFactory drinkFactory;
+        private TableFactory tableFactory;
+        private decimal totalIncome;
+
+        public RestaurantController()
+        {
+            this.menu = new List<IFood>();
+            this.drinks = new List<IDrink>();
+            this.tables = new List<ITable>();
+            this.foodFactory = new FoodFactory();
+            this.drinkFactory = new DrinkFactory();
+            this.tableFactory = new TableFactory();
+        }
+
+        public string AddFood(string type, string name, decimal price)
+        {
+            IFood food = foodFactory.CreateFood(type, name, price);
+            this.menu.Add(food);
+
+            return $"Added {name} ({type}) with price {price:f2} to the pool";
+        }
+
+        public string AddDrink(string type, string name, int servingSize, string brand)
+        {
+            IDrink drink = drinkFactory.CreateDrink(type, name, servingSize, brand);
+            this.drinks.Add(drink);
+
+            return $"Added {name} ({brand}) to the drink pool";
+        }
+
+        public string AddTable(string type, int tableNumber, int capacity)
+        {
+            ITable table = tableFactory.CreateTable(type, tableNumber, capacity);
+            this.tables.Add(table);
+
+            return $"Added table number {tableNumber} in the restaurant";
+        }
+
+        public string ReserveTable(int numberOfPeople)
+        {
+            ITable table = this.tables.FirstOrDefault(x => !x.IsReserved && x.Capacity >= numberOfPeople);
+
+            if (table == null)
+            {
+                return $"No available table for {numberOfPeople} people";
+            }
+            else
+            {
+                table.Reserve(numberOfPeople);
+                return $"Table {table.TableNumber} has been reserved for {numberOfPeople} people";
+            }
+        }
+
+        public string OrderFood(int tableNumber, string foodName)
+        {
+            ITable table = this.tables.FirstOrDefault(x => x.TableNumber == tableNumber);
+
+            if (table == null)
+            {
+                return $"Could not find table with {tableNumber}";
+            }
+
+            IFood food = this.menu.FirstOrDefault(x => x.Name == foodName);
+
+            if (food == null)
+            {
+                return $"No {foodName} in the menu";
+            }
+
+            table.OrderFood(food);
+            return $"Table {tableNumber} ordered {foodName}";
+        }
+
+        public string OrderDrink(int tableNumber, string drinkName, string drinkBrand)
+        {
+            ITable table = this.tables.FirstOrDefault(x => x.TableNumber == tableNumber);
+
+            if (table == null)
+            {
+                return $"Could not find table with {tableNumber}";
+            }
+
+            IDrink drink = this.drinks.FirstOrDefault(x => x.Name == drinkName && x.Brand == drinkBrand);
+
+            if (drink == null)
+            {
+                return $"There is no {drinkName} {drinkBrand} available";
+            }
+
+            table.OrderDrink(drink);
+            return $"Table {tableNumber} ordered {drinkName} {drinkBrand}";
+        }
+
+        public string LeaveTable(int tableNumber)
+        {
+            ITable table = this.tables.FirstOrDefault(x => x.TableNumber == tableNumber);
+
+            var bill = table.GetBill();
+            this.totalIncome += bill;
+            table.Clear();
+
+            var sb = new StringBuilder();
+            sb.AppendLine($"Table: {tableNumber}");
+            sb.Append($"Bill: {bill:f2}");
+
+            return sb.ToString();
+        }
+
+        public string GetFreeTablesInfo()
+        {
+            return string.Join(Environment.NewLine, this.tables.Where(x => x.IsReserved == false)
+                .Select(x => x.GetFreeTableInfo()));
+        }
+
+        public string GetOccupiedTablesInfo()
+        {
+            return string.Join(Environment.NewLine, this.tables.Where(x => x.IsReserved == true)
+                .Select(x => x.GetOccupiedTableInfo()));
+        }
+
+        public string GetSummary()
+        {
+            return $"Total income: {totalIncome:f2}lv";
+        }
+    }
+}
